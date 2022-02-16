bce4b3cc84cb7a2d5eb35100e97095bec3444443
omerz <omermunk95@gmail.com>
2022-02-16 02:20:32 +0200

    omer 16/02/2022 2:20

---
+++ meta_app/static/meta_app/script2.js
@@ -0,0 +1,13 @@
+
+
+function myFunction(y) {
+    y.classList.toggle("change");
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
