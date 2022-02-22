11e8dc878d0599ceb9164b73c63657fa5e0188ff
benbuaron1 <ben.buaron1@gmail.com>
2022-02-22 22:41:40 +0200

    Ben push to git 20.2

--- meta_app/urls.py
+++ meta_app/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('personal_page',views.personal_page),
     path('contact_us',views.contact_us,name='contact_us'),
     path('create_profile',views.create_profile,name='create_profile'),
-    # path('students',views.students,name='students'),
-    # path('delete_student/<int:student_id>',views.delete_student_func,name='delete_student'),
     path('edit_profile',views.edit_profile,name='edit_profile'),
 
 ]
--- meta_app/views.py
+++ meta_app/views.py
@@ -33,11 +33,7 @@ def sign_up(request):
         form = UserCreationForm()
     return render(request, 'sign_up.html', {'form': form})
 
-# class LoginView(SuccessURLAllowedHostsMixin, FormView):
-#
-#     template_name = 'index2.html'
 
-# @admin_only
 def index2(request):
     return render(request, 'index2.html')
 
@@ -54,36 +50,26 @@ def personal_page(request):
         lessons = StudentTeacherLesson.objects.filter(student=request.user.profile)
         lessons = lessons.order_by('-lesson_date')
         teachers_set = set([l.teacher for l in lessons])
-        # teachers_set = None
-        # if 'searchTeacher' in request.GET and request.GET['searchTeacher']:
-        #     if request.GET['searchTeacher'] in teachers_set: # todo Need to be fixed
-        #         teachers_set = request.GET['searchTeacher']
         return render(request,'student_page.html',context={'lessons':lessons,'teachers':teachers_set})
-
     elif group == 'teacher':
         lessons = StudentTeacherLesson.objects.filter(teacher=request.user.profile)
         lessons = lessons.order_by('-lesson_date')
         students_set = set([l.student for l in lessons])
         return render(request,'teacher_page.html',{'lessons':lessons,'students':students_set})
-
     return render(request, 'personal_page.html', {'profile': profile})
 
 def edit_profile(request):
     profile = Profile.user
     if request.method == 'POST':
         form = ProfilePageForm(instance=request.user.profile,data=request.POST,initial={'user': request.user})
-        # print('post')
-        # form.instance.user = request.user
         if form.is_valid():
             form.save()
             return redirect('/personal_page')
     elif request.method == 'GET':
         form = ProfilePageForm(instance=request.user.profile,initial={'user': request.user})
-        # print('get')
     return render(request,'edit_profile.html',{'form':form})
 
 def contact_us(request):
-
     if request.method == 'GET':
         form = ContactUs()
         return render(request, 'contact_us.html', {'form': form})
@@ -99,7 +85,7 @@ def contact_us(request):
         return redirect('/')
 
 
-
+@login_required
 def create_profile(request):
     if request.method == 'GET':
         form = ProfilePageForm(initial={'user': request.user})
