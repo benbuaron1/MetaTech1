ee912a00c7ef31ff00c03b21b85229fb59373da4
omerz <omermunk95@gmail.com>
2022-02-24 09:21:36 +0200

    omer 24.02 09:21

--- metaproject/settings.py
+++ metaproject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'MetaTech',
         'USER': 'postgres',
-        'PASSWORD': 'Benbuaron',
+        'PASSWORD': 'Daxterdark1',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
