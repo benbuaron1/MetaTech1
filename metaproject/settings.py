ae3f62ff4cfd9515a3d1aaeaa6765f595127495c
benbuaron1 <ben.buaron1@gmail.com>
2022-02-28 09:08:47 +0200

    Ben 28.2.22 9:08

--- metaproject/settings.py
+++ metaproject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'MetaTech',
         'USER': 'postgres',
-        'PASSWORD': 'Daxterdark1',
+        'PASSWORD': 'Benbuaron',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
