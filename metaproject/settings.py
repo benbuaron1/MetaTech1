7354ef83b701088eda22d88736a148ded552a89f
benbuaron1 <ben.buaron1@gmail.com>
2022-03-06 15:22:22 +0200

    Ben 28.2.22 9:08

--- metaproject/settings.py
+++ metaproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8-1u!w1ytow9n^cerdn5n0p1e^hj=mb&o$*2t_fit%y@$^=q#-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
